7f6b9835f9ec91f82d14b4ee40ef239a28d21746
julio.lopes@outsystems.com <julio.lopes@outsystems.com>
2020-03-27 18:54:16 +0000

    Update Components list

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { default as Editable } from "./components/Editable/Editable";
 export { default as Overlay } from "./components/Overlay/Overlay";
-export { default as PortalWindow } from "./components/PortalWindow/PortalWindow";
-export { default as Switch } from "./components/Switch/Switch";
